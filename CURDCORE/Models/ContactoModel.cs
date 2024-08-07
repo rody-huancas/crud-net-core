cca8c862f7fa5e9c25262e0a2574cfbfe3aa8a3f
Rody Huancas <rodyhuancas.04@gmail.com>
2024-08-06 19:19:32 -0500

    Agregar archivos de proyecto.

---
+++ CURDCORE/Models/ContactoModel.cs
@@ -0,0 +1,11 @@
+﻿namespace CURDCORE.Models
+{
+    public class ContactoModel
+    {
+        public int IdContacto { get; set; }
+        public string? Name { get; set; }
+        public string? Telefono { get; set; }
+        public string? Correo { get; set; }
+
+    }
+}
---
+++ CURDCORE/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CURDCORE.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
