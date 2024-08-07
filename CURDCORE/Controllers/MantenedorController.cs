7363a7970cc1a46bcff7b5e5d0f4fd6a0afaa68f
Rody Huancas <rodyhuancas.04@gmail.com>
2024-08-06 21:45:19 -0500

    Feat: methods for views

---
+++ CURDCORE/Controllers/MantenedorController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CURDCORE.Datos;
+using CURDCORE.Models;
+
+namespace CURDCORE.Controllers
+{
+    public class MantenedorController : Controller
+    {
+        ContactoDatos _ContactoDatos = new ContactoDatos();
+
+        public IActionResult Listar()
+        {
+            var oLista = _ContactoDatos.Listar();
+            return View(oLista);
+        }
+
+        public IActionResult Guardar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Guardar(ContactoModel oContacto)
+        {
+            var respuesta = _ContactoDatos.Guardar(oContacto);
+
+            if (respuesta)
+                return RedirectToAction("Listar");
+            else
+                return View();
+        }
+    }
+}
